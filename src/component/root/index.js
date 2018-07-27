99df77532245423f6f604f613aa6ce71bb5a0d09
luochuanjing <luochuanjing@intoms.cn>
2018-07-27 17:52:52 +0800

    修改

--- src/component/root/index.js
+++ src/component/root/index.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react'
 import './index.css'
-import { connect } from 'react-redux'
-import { httpDoubanReleasedList } from '../../action/douban'
 
-@connect(
-  (state, ownProps) => {
-    return {
-      doubanReleasedList: state.doubanReleasedList
-    }
-  },
-  (dispatch, ownProps) => {
-    return {
-      httpDoubanReleasedList: () => dispatch(httpDoubanReleasedList())
-    }
-  }
-)
 export default class App extends Component {
-  componentDidMount() {
-    this.props.httpDoubanReleasedList()
-  }
-
   render() {
-    return <div>{JSON.stringify(this.props.doubanReleasedList)}</div>
+    return <div>
+      
+    </div>
   }
 }
